ab03338682338b0e549774eec2a486683b415edf
Alexandra R <alexandra.ricardel@gmail.com>
2020-11-22 01:28:18 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -31,7 +31,15 @@ def literal_aoh
 end
 
 def aoh_lookup(aoh, row, key)
-  
+   literal_array = [
+    {name: "Don Gately", occupation: "Live-in Staff"},
+    {name: "Joelle van Dyne", occupation: "Radio Personality"},
+    {name: "Pat Monteseian", occupation: "Staff"},
+    {name: "Kate Gompert", occupation: "None"},
+    {name: "Bruce Green", occupation: "Fan of Mildred"}
+    ]
+    
+    literal_array[2][name:]
 end
 
 def aoh_update(aoh, row, key, new_value)
